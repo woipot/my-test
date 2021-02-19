52dbfbb05397cc60ae4d86983f3bbc99eb1f49dc
Woipot <frostik0409@gmail.com>
2021-02-19 21:49:06 +0300

    add fibonachi super

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def calсulate_fibonacci(first_number, iterationsCount):
         result.append(next_number)
     return result
 
+def calсulate_fibonacci_super(first_number, iterationsCount):
+    result = [first_number, 1]
+    while not len(result) == iterationsCount:
+        last_number = len(result) - 1
+        next_number = result[last_number] + result[last_number - 1]
+        result.append(next_number)
+    return result
 
 file_data = ['sum 10 10 5', 'chars df12312312tdfyt', 'mult 10 10 12', 'chars jkgbjh', 'chars', 'fibonacсi 1 7',
              'decimal  afaw 7']
@@ -36,6 +43,9 @@ for line in input_file:  # input_file:
             print(f'Кол-во букв: {char_count}')
         elif command == 'fibonacсi':
             fibonacci = calсulate_fibonacci(int(parametrs[1]), int(parametrs[2]))
+            print(f'Fibonacci: {fibonacci}')
+        elif command == 'fibonacсi_super':
+            fibonacci = calсulate_fibonacci_super(int(parametrs[1]), int(parametrs[2]))
             print(f'Fibonacci: {fibonacci}')
         else:
             raise Exception(f'Command {parametrs[0]} not found')
